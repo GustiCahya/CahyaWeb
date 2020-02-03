d693179d15fc1f6d33b67e198416c9dbf725d6c0
Gusti Cahya <gusticahyax@yahoo.com>
2020-02-03 17:05:37 +0800

    edit config

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/cahyaweb/';
+$config['base_url'] = 'https://cahyaweb.com/';
 
 /*
 |--------------------------------------------------------------------------
