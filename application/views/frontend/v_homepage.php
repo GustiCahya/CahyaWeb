2fdc39bf96caf468ab5530885eddb88b305afe0f
Gusti Cahya <gusticahyax@yahoo.com>
2020-02-03 17:06:55 +0800

    edit homepage

--- application/views/frontend/v_homepage.php
+++ application/views/frontend/v_homepage.php
@@ -75,7 +75,7 @@
                     </div>
                     <div class="row justify-content-around mb-2">
                         <div class="col-lg-4">
-                            <a href="<?=base_url().'index.php/pribadi'?>">
+                            <a href="<?=base_url().'pribadi'?>">
                                 <div class="card">
                                     <div class="card-body text-white" style="background:url('<?=base_url('assets_frontend')?>/image/paket/pribadi.jpg') no-repeat; background-size: cover; height: 200px;">
                                         <h4>Website Pribadi
@@ -88,7 +88,7 @@
                             </a>
                         </div>
                         <div class="col-lg-4">
-                            <a href="<?=base_url().'index.php/bisnis'?>">
+                            <a href="<?=base_url().'bisnis'?>">
                                 <div class="card">
                                     <div class="card-body text-white" style="background:url('<?=base_url('assets_frontend')?>/image/paket/bisnis.jpg') no-repeat; background-size: cover; height: 200px;">
                                         <h4>Website Bisnis</h4>
@@ -100,7 +100,7 @@
                             </a>
                         </div>
                         <div class="col-lg-4">
-                            <a href="<?=base_url().'index.php/ecommerce'?>">
+                            <a href="<?=base_url().'ecommerce'?>">
                                 <div class="card">
                                 <div class="card-body text-white" style="background:url('<?=base_url('assets_frontend')?>/image/paket/ecommerce.jpg') no-repeat; background-size: cover; height: 200px;">
                                     <h4>Website E-Commerce</h4>
